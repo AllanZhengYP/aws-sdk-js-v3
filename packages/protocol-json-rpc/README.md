8a20ed02e0ef9ae14e6aff45c18faf4a474847cd
Allan Zheng <zheallan@amazon.com>
2019-09-02 10:55:25 -0700

    feat: rename rest-json-protocol to protocol-json-rpc

--- packages/protocol-json-rpc/README.md
+++ packages/protocol-json-rpc/README.md
@@ -1,4 +1,4 @@
-# @aws-sdk/protocol-json-rpc
+# @aws-sdk/@aws-sdk/http-protocol
 
-[![NPM version](https://img.shields.io/npm/v/@aws-sdk/protocol-json-rpc/preview.svg)](https://www.npmjs.com/package/@aws-sdk/protocol-json-rpc)
-[![NPM downloads](https://img.shields.io/npm/dm/@aws-sdk/protocol-json-rpc.svg)](https://www.npmjs.com/package/@aws-sdk/protocol-json-rpc)
+[![NPM version](https://img.shields.io/npm/v/@aws-sdk/@aws-sdk/http-protocol/preview.svg)](https://www.npmjs.com/package/@aws-sdk/@aws-sdk/http-protocol)
+[![NPM downloads](https://img.shields.io/npm/dm/@aws-sdk/@aws-sdk/http-protocol.svg)](https://www.npmjs.com/package/@aws-sdk/@aws-sdk/http-protocol)
--- packages/protocol-json-rpc/src/index.ts
+++ packages/protocol-json-rpc/src/index.ts
@@ -1,2 +1,33 @@
-export * from "./JsonRpcParser";
-export * from "./JsonRpcSerializer";
+import {
+  RequestSerializer,
+  ResponseParser,
+  ParserDecoratorStack
+} from "@aws-sdk/types";
+import { Protocol, TransferHandler } from "@aws-sdk/protocol";
+
+export class JsonRpcProtocol<StreamType> extends Protocol<
+  HttpRequest<StreamType>,
+  HttpResponse<StreamType>
+> {
+  constructor(
+    handler: TransferHandler<HttpRequest<StreamType>, HttpResponse<StreamType>>
+  ) {
+    super(handler);
+  }
+  serialize(
+    serializer: RequestSerializer<HttpRequest<StreamType>>,
+    input: any
+  ) {
+    return serializer(input, "JsonRpc");
+  }
+  parse(
+    parser: ResponseParser<HttpResponse<StreamType>>,
+    output: HttpResponse<StreamType>,
+    decoratorStack?: ParserDecoratorStack<HttpResponse<StreamType>>
+  ) {
+    if (decoratorStack) {
+      output = decoratorStack.resolve()(output);
+    }
+    return parser(output, "JsonRpc") as any;
+  }
+}
