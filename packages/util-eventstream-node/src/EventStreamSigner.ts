984a1be55c2aded3ee5f9a983ef2de9368e645aa
Allan Zheng <zheallan@amazon.com>
2020-01-29 09:28:44 -0800

    feat: allow event signer provider in the config

--- packages/util-eventstream-node/src/EventStreamSigner.ts
+++ packages/util-eventstream-node/src/EventStreamSigner.ts
@@ -1,16 +1,22 @@
-import { EventSigner, HttpRequest, Encoder, Decoder } from "@aws-sdk/types";
+import {
+  EventSigner,
+  HttpRequest,
+  Encoder,
+  Decoder,
+  Provider
+} from "@aws-sdk/types";
 import { EventStreamMarshaller as EventMarshaller } from "@aws-sdk/eventstream-marshaller";
 import { pipeline } from "stream";
 import { EventSigningStream } from "./EventSigningStream";
 
 export interface EventStreamRequestSignerOptions {
-  eventSigner: EventSigner;
+  eventSigner: EventSigner | Provider<EventSigner>;
   utf8Encoder: Encoder;
   utf8Decoder: Decoder;
 }
 
 export class EventStreamRequestSigner {
-  private readonly eventSigner: EventSigner;
+  private readonly eventSigner: EventSigner | Provider<EventSigner>;
   private readonly eventMarshaller: EventMarshaller;
   constructor(options: EventStreamRequestSignerOptions) {
     this.eventSigner = options.eventSigner;
@@ -19,14 +25,17 @@ export class EventStreamRequestSigner {
       options.utf8Decoder
     );
   }
-  sign(request: HttpRequest): Promise<HttpRequest> {
+  async sign(request: HttpRequest): Promise<HttpRequest> {
     const signature = request.headers["authorization"].match(
       /Signature=([\w]+)$/
     )![1];
     const signingStream = new EventSigningStream({
       priorSignature: signature,
       eventMarshaller: this.eventMarshaller,
-      eventSigner: this.eventSigner
+      eventSigner:
+        typeof this.eventSigner === "function"
+          ? await this.eventSigner()
+          : this.eventSigner
     });
     pipeline(request.body, signingStream, err => {
       if (err) {
