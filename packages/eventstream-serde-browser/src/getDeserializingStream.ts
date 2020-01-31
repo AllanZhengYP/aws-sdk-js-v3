33b4b26440705cd1243b9c511d0e9eed1bc0c1f8
Allan Zheng <zheallan@amazon.com>
2020-01-30 17:25:08 -0800

    feat: add eventstream-serde-browser

---
+++ packages/eventstream-serde-browser/src/getDeserializingStream.ts
@@ -0,0 +1,26 @@
+import { Message } from "@aws-sdk/types";
+
+export function getDeserializingStream(
+  messageStream: ReadableStream<{ [name: string]: Message }>,
+  deserializer: (input: any) => any
+): ReadableStream<{ [name: string]: any }> {
+  const messageReader = messageStream.getReader();
+  const deserializedStream = new ReadableStream<{ [name: string]: any }>({
+    start(controller) {
+      function push() {
+        messageReader.read().then(async ({ done, value }) => {
+          if (done) {
+            controller.close();
+            return;
+          }
+
+          controller.enqueue(await deserializer(value));
+          push();
+        });
+      }
+
+      push();
+    }
+  });
+  return deserializedStream;
+}
