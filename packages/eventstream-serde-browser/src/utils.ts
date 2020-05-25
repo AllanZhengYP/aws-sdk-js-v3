8037303541c54ceec040790a5c981a845a5e3445
Allan Zheng <zheallan@amazon.com>
2020-05-25 11:29:40 -0700

    feat(eventstream-serde-browser): make it based on universal es serde
    Now this package can fully serialize & deserailize eventstream with web
    APIs. In browsers, it converts from user-side iterables of input structure
    to internal ReadableStream of binaries. In ReactNative, it converts from
    user-side iterables of input structure to internal iterables of binaries.
    The same applies to the other converting direction.

--- packages/eventstream-serde-browser/src/utils.ts
+++ packages/eventstream-serde-browser/src/utils.ts
@@ -1,19 +1,40 @@
 /**
  * Convert ReadableStream into an async iterable.
+ * Reference: https://jakearchibald.com/2017/async-iterators-and-generators/#making-streams-iterate
  */
 export async function* ReadableStreamtoIterable<T>(
-  readableStream: ReadableStream
+  readableStream: ReadableStream<T>
 ): AsyncIterable<T> {
   const reader = readableStream.getReader();
-  let done = false;
-  while (!done) {
-    const { done: end, value } = await reader.read();
-    if (end) {
-      done = true;
-      break;
+  try {
+    while (true) {
+      // Read from the stream
+      const { done, value } = await reader.read();
+      // Exit if we're done
+      if (done) return;
+      // Else yield the chunk
+      yield value as T;
     }
-    if (value) {
-      yield value;
-    }
+  } finally {
+    reader.releaseLock();
   }
+}
+
+/**
+ * Convert async iterable to a ReadableStream.
+ */
+export function iterableToReadableStream<T>(
+  asyncIterable: AsyncIterable<T>
+): ReadableStream<T> {
+  const iterator = asyncIterable[Symbol.asyncIterator]();
+  return new ReadableStream({
+    pull(constroller) {
+      return iterator.next().then(({ done, value }) => {
+        if (done) {
+          return constroller.close();
+        }
+        constroller.enqueue(value);
+      });
+    }
+  });
 }
