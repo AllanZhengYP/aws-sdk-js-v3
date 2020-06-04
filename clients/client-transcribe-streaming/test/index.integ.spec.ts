0cdcbbef40151b9aeee5f9b70ae8fbc635dbed26
Allan Zheng <zheallan@amazon.com>
2020-06-04 12:16:34 -0700

    test(client-transcribe-streaming): update node integ test

--- clients/client-transcribe-streaming/test/index.integ.spec.ts
+++ clients/client-transcribe-streaming/test/index.integ.spec.ts
@@ -1,7 +1,6 @@
 import { TranscribeStreaming } from "../index";
 import { createReadStream } from "fs";
 import { join } from "path";
-const audio = createReadStream(join(__dirname, "speech.wav"));
 
 describe("TranscribeStream client", () => {
   const client = new TranscribeStreaming({});
@@ -12,13 +11,18 @@ describe("TranscribeStream client", () => {
   it("should stream the transcript", async () => {
     const LanguageCode = "en-US";
     const MediaEncoding = "pcm";
-    const MediaSampleRateHertz = 44100;
+    const MediaSampleRateHertz = 8000;
     const result = await client.startStreamTranscription({
       LanguageCode,
       MediaEncoding,
       MediaSampleRateHertz,
       AudioStream: (async function* () {
-        for await (const chunk of audio) {
+        for await (const chunk of createReadStream(
+          join(__dirname, "speech.wav"),
+          {
+            highWaterMark: 10 * 1000
+          }
+        )) {
           yield { AudioEvent: { AudioChunk: chunk } };
         }
       })(),
@@ -27,10 +31,19 @@ describe("TranscribeStream client", () => {
     expect(result.MediaEncoding).toBe(MediaEncoding);
     expect(result.MediaSampleRateHertz).toBe(MediaSampleRateHertz);
     expect(result.TranscriptResultStream).toBeDefined();
-    const transcripts = [];
+    let transcripts = "";
     for await (const event of result.TranscriptResultStream!) {
-      transcripts.push(event);
+      const results = event.TranscriptEvent?.Transcript?.Results;
+      if (
+        results &&
+        results.length > 0 &&
+        results[0].Alternatives &&
+        results[0].Alternatives.length > 0
+      ) {
+        const transcript = `${results[0].Alternatives[0].Transcript}\n`;
+        transcripts += transcript;
+      }
     }
-    expect(transcripts.filter((event) => event["TranscriptEvent"]).length).toBeGreaterThan(0);
+    expect(transcripts.indexOf("But what if somebody") > -1).toBe(true);
   }, 60000);
 });
